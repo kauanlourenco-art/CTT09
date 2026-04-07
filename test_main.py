411ab6943f0795553c196ee2eb28ee9a062a37a1
kauan <kauan.lourenco@aluno.impacta.edu.br>
2026-04-06 22:12:43 -0300

    adicionar rota multiplicar e falhar

--- test_main.py
+++ test_main.py
@@ -12,3 +12,9 @@ def test_somar():
     response = client.get("/somar/5/3")
     assert response.status_code == 200
     assert response.json() == {"resultado": 8}
+
+def test_multiplicar():
+    response = client.get("/multiplicar/2/2")
+    assert response.status_code == 200
+    # Erro intencional (2 * 2 não é 5)
+    assert response.json() == {"resultado": 5}
